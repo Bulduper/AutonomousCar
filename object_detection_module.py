c360a39f3788eaec3086cea5bf7f46a31de854f9
bulduper <dawid199912@gmail.com>
2022-06-24 00:36:07 +0200

    readme update

--- object_detection_module.py
+++ object_detection_module.py
@@ -4,7 +4,7 @@ import numpy as np
 # frameGPU_bgr_small_detection = jetson.utils.cudaAllocMapped(width=640, height=480, format='bgr8')
 frameGPU_bgr_small_detection = jetson.utils.cudaAllocMapped(width=int(1640*0.6), height=int(1232*0.2), format='bgr8')
 # frameGPU_bgr_small_detection = jetson.utils.cudaAllocMapped(width=int(1640*1), height=int(1232*1), format='bgr8')
-MODEL_PATH = './networks/signs_raw_v0_3.onnx'
+MODEL_PATH = './networks/signs_raw_v0_4.onnx'
 # MODEL_PATH = './networks/model4_raw_only_2000s_ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.onnx'
 THRESHOLD = 0.6
 
